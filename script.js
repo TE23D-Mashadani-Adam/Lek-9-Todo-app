21b624d8a0eefafe99335decdf48a6d2c0503f60
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2025-01-17 14:26:46 +0100

    Lagt till några funktioner, max 5 tasks och man kan ta bort tasks

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let listaUl = document.querySelector("ul");
 
+let maxValue = 5;
+let currentValue = 0;
+
+
 
 function AddList() {
-    let task = document.createElement("li");
-    let input = document.querySelector("input");
-    console.log(input);
-    task.textContent = input.value;
-    task.classList.add("list_css");
-    listaUl.appendChild(task);
-    input.value = "";
+    if (currentValue < maxValue) {
+        let task = document.createElement("li");
+        let input = document.querySelector("input");
+        console.log(input);
+        task.textContent = input.value;
+        task.classList.add("list_css");
+        if (input.value != "") {
+            listaUl.appendChild(task);
+        }else{
+            alert("You can not add empty task")
+        }
+        input.value = "";
+        currentValue++;
+    }
+    else {
+        alert("You can add max 5 tasks!")
+    }
+}
+
+function Remove_List() {
+    listaUl.removeChild(listaUl.lastChild);
+    currentValue--;
 }
